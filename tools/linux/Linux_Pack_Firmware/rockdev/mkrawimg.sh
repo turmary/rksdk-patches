1194f83b21f2c8ae64a3111a971bf6581c78311c
turmary <turmary@126.com>
2022-01-01 23:23:26 +0800

    Update: mkrawimg.sh flash SD Card bootup OK

--- tools/linux/Linux_Pack_Firmware/rockdev/mkrawimg.sh
+++ tools/linux/Linux_Pack_Firmware/rockdev/mkrawimg.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-RAWIMG=raw.img
-echo "Generate raw image : ${RAWIMG} !"
+
+CMD=$(realpath $0)
+IMG_DIR=`dirname $CMD`
+cd $IMG_DIR
+
+RAWIMG=Image/raw.img
+echo "Generate RAW image : ${RAWIMG} !"
 
 LOADER1_START=64
 PARAMETER=$(cat package-file | grep -wi parameter | awk '{printf $2}' | sed 's/\r//g')
@@ -11,44 +16,8 @@ PARTITION_INDEX=0
 
 rm -rf ${RAWIMG}
 
-ALIGN()
-{
-	X=$1
-	A=$2
-	OUT=$(($((${X} + ${A} -1 ))&$((~$((${A}-1))))))
-	printf 0x%x ${OUT}
-}
-
-ROOTFS_LAST=$(grep "rootfs:grow" Image/parameter.txt)
-if [ -z "${ROOTFS_LAST}" ]
-then
-echo "Resize rootfs partition size"
-FILE_P=$(readlink -f Image/rootfs.img)
-FS_INFO=$(dumpe2fs -h ${FILE_P})
-BLOCK_COUNT=$(echo "${FS_INFO}" | grep "^Block count" | cut -d ":" -f 2 | tr -d "[:blank:]")
-INODE_COUNT=$(echo "${FS_INFO}" | grep "^Inode count" | cut -d ":" -f 2 | tr -d "[:blank:]")
-BLOCK_SIZE=$(echo "${FS_INFO}" | grep "^Block size" | cut -d ":" -f 2 | tr -d "[:blank:]")
-INODE_SIZE=$(echo "${FS_INFO}" | grep "^Inode size" | cut -d ":" -f 2 | tr -d "[:blank:]")
-BLOCK_SIZE_IN_S=$((${BLOCK_SIZE}>>9))
-INODE_SIZE_IN_S=$((${INODE_SIZE}>>9))
-SKIP_BLOCK=70
-EXTRA_SIZE=$(expr 50 \* 1024 \* 2 ) #50M
-
-FSIZE=$(expr ${BLOCK_COUNT} \* ${BLOCK_SIZE_IN_S} + ${INODE_COUNT} \* ${INODE_SIZE_IN_S} + ${EXTRA_SIZE} + ${SKIP_BLOCK})
-PSIZE=$(ALIGN $((${FSIZE})) 512)
-PARA_FILE=$(readlink -f Image/parameter.txt)
-
-ORIGIN=$(grep -Eo "0x[0-9a-fA-F]*@0x[0-9a-fA-F]*\(rootfs" $PARA_FILE)
-NEWSTR=$(echo $ORIGIN | sed "s/.*@/${PSIZE}@/g")
-OFFSET=$(echo $NEWSTR | grep -Eo "@0x[0-9a-fA-F]*" | cut -f 2 -d "@")
-NEXT_START=$(printf 0x%x $(($PSIZE + $OFFSET)))
-sed -i.orig "s/$ORIGIN/$NEWSTR/g" $PARA_FILE
-sed -i "/^CMDLINE.*/s/-@0x[0-9a-fA-F]*/-@$NEXT_START/g" $PARA_FILE
-fi
-
 for PARTITION in `cat ${PARAMETER} | grep '^CMDLINE' | sed 's/ //g' | sed 's/.*:\(0x.*[^)])\).*/\1/' | sed 's/,/ /g'`; do
-        PARTITION_NAME=`echo ${PARTITION} | sed 's/\(.*\)(\(.*\))/\2/' | awk -F : {'print $1'}`
-        PARTITION_FLAG=`echo ${PARTITION} | sed 's/\(.*\)(\(.*\))/\2/' | awk -F : {'print $2'}`
+        PARTITION_NAME=`echo ${PARTITION} | sed 's/\(.*\)(\(.*\))/\2/' | cut -f 1 -d ":"`
         PARTITION_START=`echo ${PARTITION} | sed 's/.*@\(.*\)(.*)/\1/'`
         PARTITION_LENGTH=`echo ${PARTITION} | sed 's/\(.*\)@.*/\1/'`
 
@@ -56,9 +25,9 @@ for PARTITION in `cat ${PARAMETER} | grep '^CMDLINE' | sed 's/ //g' | sed 's/.*:
         PARTITION_INDEX=$(expr $PARTITION_INDEX + 1)
 
         eval "${PARTITION_NAME}_START_PARTITION=${PARTITION_START}"
-        eval "${PARTITION_NAME}_FLAG_PARTITION=${PARTITION_FLAG}"
         eval "${PARTITION_NAME}_LENGTH_PARTITION=${PARTITION_LENGTH}"
         eval "${PARTITION_NAME}_INDEX_PARTITION=${PARTITION_INDEX}"
+        printf "    %-15s\t%10s\t%10s\n" "$PARTITION_NAME" "$PARTITION_START" "$PARTITION_LENGTH"
 done
 
 LAST_PARTITION_IMG=$(cat package-file | grep -wi $PARTITION_NAME | awk '{printf $2}' | sed 's/\r//g')
@@ -77,11 +46,9 @@ parted -s ${RAWIMG} mklabel gpt
 
 for PARTITION in ${PARTITIONS[@]}; do
     PSTART=${PARTITION}_START_PARTITION
-    PFLAG=${PARTITION}_FLAG_PARTITION
     PLENGTH=${PARTITION}_LENGTH_PARTITION
     PINDEX=${PARTITION}_INDEX_PARTITION
     PSTART=${!PSTART}
-    PFLAG=${!PFLAG}
     PLENGTH=${!PLENGTH}
     PINDEX=${!PINDEX}
 
@@ -92,12 +59,7 @@ for PARTITION in ${PARTITIONS[@]}; do
         PEND=$(((${PSTART} + 0x00 + ${PLENGTH})))
         parted -s ${RAWIMG} unit s mkpart ${PARTITION} $(((${PSTART} + 0x00))) $(expr ${PEND} - 1)
     fi
-
-    if [ "${PFLAG}"x == "bootable"x ];then
-        parted -s ${RAWIMG} set $PINDEX legacy_boot on
-    fi
 done
-
 
 UUID=$(cat ${PARAMETER} | grep 'uuid' | cut -f 2 -d "=")
 VOL=$(cat ${PARAMETER} | grep 'uuid' | cut -f 1 -d "=" | cut -f 2 -d ":")
@@ -114,8 +76,10 @@ y
 EOF
 
 if [ "$RK_IDBLOCK_UPDATE" = "true" ]; then
+	echo -e "\e[33mLOADER1 Image/idblock.bin ${LOADER1_START}\e[0m"
 	dd if=Image/idblock.bin of=${RAWIMG} seek=${LOADER1_START} conv=notrunc
 else
+	echo -e "\e[33mLOADER1 Image/idbloder.img ${LOADER1_START}\e[0m"
 	dd if=Image/idbloader.img of=${RAWIMG} seek=${LOADER1_START} conv=notrunc
 fi
 
@@ -127,7 +91,7 @@ for PARTITION in ${PARTITIONS[@]}; do
 
     if [[ x"$IMGFILE" != x ]]; then
 		if [[ -f "$IMGFILE" ]]; then
-			echo ${PARTITION} ${IMGFILE} ${PSTART}
+			echo -e "\e[33m${PARTITION} ${IMGFILE} ${PSTART}\e[0m"
 			dd if=${IMGFILE}  of=${RAWIMG} seek=$(((${PSTART} + 0x00))) conv=notrunc,fsync
 		else
 			if [[ x"$IMGFILE" != xRESERVED ]]; then
@@ -137,12 +101,12 @@ for PARTITION in ${PARTITIONS[@]}; do
     fi
 done
 
-if [ -e ${PARA_FILE}.orig ]
-then
-	mv ${PARA_FILE}.orig ${PARA_FILE}
-	exit $?
-else
-	exit 0
-fi
+RAWIMG=$(realpath $RAWIMG)
+[ -s ${RAWIMG} ] || {
+	echo "Make ${RAWIMG} failed"
+	exit 1
+}
 
-echo "mk raw img OK"
+echo -e "\n\e[5;30;42mMake RAW image ${RAWIMG} OK\e[0m"
+
+
